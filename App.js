e6abdf1af0bc5d91226ccc482740ed1b11f38c90
sarveshkore <sarvesh2k2@gmail.com>
2024-05-22 17:30:13 +0530

    Understanding JSX and React Components

--- App.js
+++ App.js
@@ -4,9 +4,28 @@ import ReactDOM from 'react-dom/client'
 console.log("React",React); // React { version: '16.13.1', ... }
 console.log("ReactDOM",ReactDOM);
 
-const heading1= React.createElement("h1",{className:"heading1"},"1.CSK");
+const heading1= React.createElement("h1",{className:"heading1"},"1.CSK");   //React Element
+console.log("heading1",heading1);
+
+const heading11 = <h1 className='heading1'>Heading11</h1>;      //React Element using JSX
+console.log("heading 11",heading11);
+
+//FUNCTION BASED COMPONENTS
+const Component = () =>{
+    return(
+        <div>I am a functional component</div>
+    )
+}
+const Component1 = () =><div>I am a functional component</div>
+
+function Component2(){
+    return(
+        <div>I am a functional component</div>
+    )
+}
+
 const heading2= React.createElement("h2",{className:"heading2"},"2.MI");
 
-const container1= React.createElement("div",{className:"container"},[heading1,heading2]);  //heading1 element appended in container1
+const container1= React.createElement("div",{className:"container"},[heading1,heading11,heading2]);  //heading1 element appended in container1
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(container1);
